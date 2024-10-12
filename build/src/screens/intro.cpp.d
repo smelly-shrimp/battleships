38fa10399a7e725d9c4d9da8409e76aff27c4a17
shrimp <tymek.lamza@op.pl>
2024-10-12 02:16:35 +0200

    added intro screen logic

--- build/src/screens/intro.cpp.d
+++ build/src/screens/intro.cpp.d
@@ -1,4 +1,6 @@
 build/./src/screens/intro.cpp.o: src/screens/intro.cpp \
-  src/screens/inc/intro.h
+  src/screens/inc/intro.h src/inc/fman.h
 
 src/screens/inc/intro.h:
+
+src/inc/fman.h:
--- src/screens/intro.cpp
+++ src/screens/intro.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
+#include <chrono>
+#include <thread>
 #include "intro.h"
+#include "game.h"
+#include "fman.h"
 
 using namespace std;
 
 void Intro::print()
 {
-    cout << "foo" << endl;
+    for (string f : Fman::read("intro")) {
+        printf("\033[2J");
+        cout << f << endl;
+        this_thread::sleep_for(chrono::milliseconds(500));
+    }
+
+    Game::setGameState(GameStates::WELCOME);
 }
